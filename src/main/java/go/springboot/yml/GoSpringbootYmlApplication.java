ff5e0ea88ed5e4f2210b17a0cd299d65a37d19ca
glenn opdycke-hansen <glennoph@gmail.com>
2021-10-11 15:03:02 -0500

    read from application.properties

--- src/main/java/go/springboot/yml/GoSpringbootYmlApplication.java
+++ src/main/java/go/springboot/yml/GoSpringbootYmlApplication.java
@@ -1,38 +1,52 @@
 package go.springboot.yml;
 
+import lombok.Value;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@Slf4j
-//@Log4j2
-//@Log fail
+@Log4j2
+
 public class GoSpringbootYmlApplication implements CommandLineRunner {
 
-	private ApplicationConfig config;
+	//private ApplicationConfig config;
 
-	public GoSpringbootYmlApplication(ApplicationConfig ymlConfig) {
-		this.config = ymlConfig;
-	}
+//	public GoSpringbootYmlApplication(ApplicationConfig ymlConfig) {
+//		this.config = ymlConfig;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoSpringbootYmlApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(Environment environment) {
+		return new ApplicationRunner() {
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+				log.info("message from application.properties " +
+						environment.getProperty("message-from-application-properties"));
+			}
+		};
+	}
+
+
 	@Override
 	public void run(String... args) throws Exception {
-//		System.out.println("environment: " + config.getEnvironment());
-//		System.out.println("name: " + config.getName());
-//		System.out.println("enabled: "+ config.isEnabled());
-		log.info("environment: {}", config.getEnvironment());
-		log.info("name: {}", config.getName());
-		log.info("enabled: {}", config.isEnabled());
 
-		log.info("servers: {}", config.getServers());
+//		log.info("environment: {}", config.getEnvironment());
+//		log.info("name: {}", config.getName());
+//		log.info("enabled: {}", config.isEnabled());
+//
+//		log.info("servers: {}", config.getServers());
 	}
 }
